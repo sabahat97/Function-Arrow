d37dff4f87f9825073f76ff679e1183feb5dfcd2
syeda <sbtshakeel61@gmail.com>
2024-04-28 22:59:06 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,5 @@
+//basic example
+let dish =() => // arrow function started from let .
+    1 + 2 + 4
+let response = dish()
+console.log (response) // output 7
---
+++ main.ts
@@ -0,0 +1,4 @@
+//advance example
+let order = (rice : string, drinks: string, icecream: string) :string => rice  +","+ drinks +","+ icecream
+let response = order("2 plates biryani","2 regular marinda","4 scope vanilla ice-cream")
+console.log (response); //output 2 plates biryani,2 regular marinda,4 scope vanilla ice-cream
